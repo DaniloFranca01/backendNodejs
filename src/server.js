4b6a363c63aa2c18eb0008718ebd5886240527ff
Danilo Franca <danilo.franca01@gmail.com>
2019-12-22 02:20:27 -0300

    integracao com o mongoDB

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
-import express, { json } from 'express';
-import routes from './routes';
+const express = require('express');
+const routes = require('./routes');
+const mongoose = require('mongoose');
 
 const app = express();
-app.use(json());
+mongoose.connect('mongodb+srv://danilo:danilo123456@omnistack-lvqzs.gcp.mongodb.net/estudo?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+app.use(express.json());
+app.use(routes);
 app.listen(3333);
