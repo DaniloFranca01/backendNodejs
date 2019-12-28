44b5edda74789e9018847fa5b1a026b44c08bdf0
Danilo Franca <danilo.franca01@gmail.com>
2019-12-28 18:50:01 -0300

    cors instalado

--- src/server.js
+++ src/server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 mongoose.connect('mongodb+srv://danilo:danilo123456@omnistack-lvqzs.gcp.mongodb.net/estudo?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.listen(3333);
